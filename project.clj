49c373e8fedb9539d10819e2b9a3357d58a1468d
Pedro Escalante <escalante308@gmail.com>
2021-05-23 20:22:11 -0500

    Add Rum HTML library - First Components

--- project.clj
+++ project.clj
@@ -6,7 +6,9 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [ring "1.8.0"]
                  [compojure "1.6.2"]
-                 [ring/ring-defaults "0.3.2"]]
+                 [ring/ring-defaults "0.3.2"]
+                 [yogthos/config "1.1.7"]
+                 [rum "0.11.4"]]
   :main ^:skip-aot friendwall.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
--- src/friendwall/core.clj
+++ src/friendwall/core.clj
@@ -1,17 +1,34 @@
 (ns friendwall.core
-  (:require [ring.adapter.jetty :refer [run-jetty]]
-            [compojure.core :refer :all]
-            [compojure.route :as route]
-            [ring.middleware.defaults :refer :all])
+  (:require [ring.adapter.jetty :refer [run-jetty]]                                                         ;; Library for Web Server
+            [compojure.core :refer :all]                                                                    ;; Brings HTTP Services
+            [compojure.route :as route]                                                                     ;; Allows Routes
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]                                 ;; Middlewares
+            [config.core :refer [env]]                                                                      ;; Config file for .env variables
+            [rum.core :refer [defc render-static-markup]])                                                  ;; Rum HTML
   (:gen-class))
 
-(defroutes app
-  (GET "/" [] "<h1>Hello World</h1>")
-  (GET "/friends" [] "<h1>There are no friends yet</h1>")
-)
+(defc template [headline component]                                                                         ;; Rum Template
+  [:div {:id "main-div"
+         :class "main-page-div"}
+   [:h1 headline]
+   [:ul {:class "nav"}
+    [:li [:a {:href "/"} "Home"]]
+    [:li [:a {:href "/friends"} "Friends"]]]
+   component])
+
+(defc main-page [req]                                                                                       ;; Rum Controller
+  [:p (str req)])
+
+(defc friends-page []                                                                                       ;; Rum Controller
+  [:p "This is the Friends page!"])
+
+(defroutes app                                                                                              ;; Route Declaration
+  (GET "/" [req] (render-static-markup (template "Hello World!" (main-page {:request req}))))               ;; GET / using main-page component
+  (GET "/friends" [] (render-static-markup (template "No friends, yet :(" friends-page))))                  ;; GET /friends using friends-page component
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (run-jetty app {:port 80})     ;; Start the Jetty server on start-up
-)
+  (run-jetty                                                                                                ;; Start the Jetty server on start-up, using app route declaration
+   (wrap-defaults app site-defaults)
+    {:port (:port env)}))                                                                                   ;; Reads the port from the /resources/config.edn file
